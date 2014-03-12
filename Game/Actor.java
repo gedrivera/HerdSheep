1bcca8dac4c089ad33e8e6936e18a4c344b0fc93
Error502 <TVoyles404@gmail.com>
2014-03-12 09:34:02 -0500

    Update Actor.java

--- Game/Actor.java
+++ Game/Actor.java
@@ -4,12 +4,13 @@ import android.location.Location;
 
 /**
  * Interface that is to be extended by all
- * pieces that is animated or controlled
- *
+ * pieces that are animated or controlled
  */
 public interface Actor
 {
 	Location respondToInput(); //Makes the actor respond to input
 	
-	Location move(); 	     	   //Movement by code
+	Location move(); 		   //Movement by code
+	
+	void despawn();		       //Remove the Actor
 }
